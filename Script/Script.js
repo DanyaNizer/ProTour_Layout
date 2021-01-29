7d54e5a82ba3afba2c90d8b4a618bec6aa83f513
Dmitry-Kirtadze <mr.dima.kirtadze@gmail.com>
2021-01-29 16:35:49 +0200

    КРЧ ПЗДЦ Но модули я поставил))

--- Script/Script.js
+++ Script/Script.js
@@ -1,23 +1,23 @@
-$(document).ready(function() {
-    var HeaderTop = $('#header').offset().top;
+$(document).ready(function () {
+	var HeaderTop = $('#header').offset().top;
 
-    $(window).scroll(function() {
-        if ($(window).scrollTop() > HeaderTop) {
-            $('#header').css({ position: 'fixed', top: '0px' });
-        } else {
-            $('#header').css({ position: 'static' });
-        }
-    });
+	$(window).scroll(function () {
+		if ($(window).scrollTop() > HeaderTop) {
+			$('#header').css({ position: 'fixed', top: '0px' });
+		} else {
+			$('#header').css({ position: 'static' });
+		}
+	});
 });
 
 "use strict";
 
 
 // загрузочный экран
-(function($) {
-    $(window).load(function() {
-        $("#loader").fadeOut("slow", function() {
-            $("#preloader").delay(3500).fadeOut("slow");
-        });
-    })
+(function ($) {
+	$(window).load(function () {
+		$("#loader").fadeOut("slow", function () {
+			$("#preloader").delay(3500).fadeOut("slow");
+		});
+	})
 })(jQuery);

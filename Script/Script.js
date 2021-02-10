3ba6c3bba0db85d759de97d4ba4a3ffdc0e2f80e
Dmitry-Kirtadze <mr.dima.kirtadze@gmail.com>
2021-02-10 15:18:08 +0200

    Добавлен слайдер

--- Script/Script.js
+++ Script/Script.js
@@ -19,3 +19,28 @@
 $(function () {
 	$('#preloader').delay(3500).fadeOut('slow');
 });
+
+
+
+// Дальше слайдер
+$('.slick__slider').slick({
+	infinite: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	arrows: true,
+	dots: true,
+	responsive: [
+		{
+			breakpoint: 768,
+			settings: {
+				arrows: false
+			}
+		},
+		{
+			breakpoint: 320,
+			settings: {
+				arrows: false
+			}
+		}
+	]
+});

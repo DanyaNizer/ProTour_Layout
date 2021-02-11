086455f029d857da9f9f3c181ec35d1a051e7723
DanyaNizer <d359753@gmail.com>
2021-02-11 19:37:55 +0200

    Индекс

--- Script/Script.js
+++ Script/Script.js
@@ -1,47 +1,45 @@
+// Липкий header
+$(document).ready(function() {
+    var HeaderTop = $('#header').offset().top;
+
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > HeaderTop) {
+            $('#header').css({ position: 'fixed', top: '0px' });
+        } else {
+            $('#header').css({ position: 'static' });
+        }
+    });
+});
 
 
-// $(document).ready(function () {
-// 	var HeaderTop = $('#header').offset().top;
-
-// 	$(window).scroll(function () {
-// 		if ($(window).scrollTop() > HeaderTop) {
-// 			$('#header').css({ position: 'fixed', top: '0px' });
-// 		} else {
-// 			$('#header').css({ position: 'static' });
-// 		}
-// 	});
-// });
 
 
-
-
-// загрузочный экран
-$(function () {
-	$('#preloader').delay(3500).fadeOut('slow');
+// Загрузочный экран
+$(function() {
+    $('#preloader').delay(3500).fadeOut('slow');
 });
 
 
 
 // Дальше слайдер
 $('.slick__slider').slick({
-	infinite: true,
-	slidesToShow: 1,
-	slidesToScroll: 1,
-	arrows: false,
-	dots: true,
-	lazyLoad: 'ondemand',
-	responsive: [
-		{
-			breakpoint: 768,
-			settings: {
-				arrows: false
-			}
-		},
-		{
-			breakpoint: 320,
-			settings: {
-				arrows: false
-			}
-		}
-	]
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    lazyLoad: 'ondemand',
+    responsive: [{
+            breakpoint: 768,
+            settings: {
+                arrows: false
+            }
+        },
+        {
+            breakpoint: 320,
+            settings: {
+                arrows: false
+            }
+        }
+    ]
 });

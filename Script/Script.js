5384f2e51b604497b5fa33d762775e6ceea7fa5f
DanyaNizer <d359753@gmail.com>
2021-02-12 14:20:16 +0200

    Готовый хедер и стартовой без адаптива

--- Script/Script.js
+++ Script/Script.js
@@ -1,47 +1,71 @@
+// Липкий header
+$(window).on('scroll', function() {
 
+    var y = $(window).scrollTop(),
+        topBar = $('.myheader');
+    topPad = $('.header__top-info-bg');
 
-// $(document).ready(function () {
-// 	var HeaderTop = $('#header').offset().top;
+    if (y > 150) {
+        topBar.addClass('sticky');
+        // topPad.addClass('sticpad');
+    } else {
+        topBar.removeClass('sticky');
+        // topPad.removeClass('sticpad');
+    }
 
-// 	$(window).scroll(function () {
-// 		if ($(window).scrollTop() > HeaderTop) {
-// 			$('#header').css({ position: 'fixed', top: '0px' });
-// 		} else {
-// 			$('#header').css({ position: 'static' });
-// 		}
-// 	});
-// });
+});
 
 
 
 
 // загрузочный экран
-$(function () {
-	$('#preloader').delay(3500).fadeOut('slow');
+$(function() {
+    $('#preloader').delay(1500).fadeOut('slow');
 });
 
 
 
 // Дальше слайдер
 $('.slick__slider').slick({
-	infinite: true,
-	slidesToShow: 1,
-	slidesToScroll: 1,
-	arrows: false,
-	dots: true,
-	lazyLoad: 'ondemand',
-	responsive: [
-		{
-			breakpoint: 768,
-			settings: {
-				arrows: false
-			}
-		},
-		{
-			breakpoint: 320,
-			settings: {
-				arrows: false
-			}
-		}
-	]
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    lazyLoad: 'ondemand',
+    responsive: [{
+            breakpoint: 768,
+            settings: {
+                arrows: false
+            }
+        },
+        {
+            breakpoint: 320,
+            settings: {
+                arrows: false
+            }
+        }
+    ]
+});
+
+
+
+$('.rewies__slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true
+})
+
+
+$(document).ready(function() {
+    $('a[href^="#down"], *[data-href^="#down"]').on('click', function(e) {
+        e.preventDefault();
+        var t = 1000;
+        var d = $(this).attr('data-href') ? $(this).attr('data-href') : $(this).attr('href');
+        $('html,body').stop().animate({ scrollTop: $(d).offset().top }, t);
+    });
 });
